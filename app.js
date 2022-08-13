0408b66977502749369b895b98cb829809c8603b
chelsac <chelsacleetuscc@gmail.com>
2022-08-13 22:10:34 +0530

    hoist

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');      
 const path = require ('path'); 
 const cors = require('cors');
-const port= 5000;
+const port= process.env.PORT || 5000;
 
 // const nav= [                         //Part #2 Point 6
 //     {
